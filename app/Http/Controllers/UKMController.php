e92ce6cdafd6c54bd2b60965c442ac9c12803365
Lola Anggita <suchcemeann@gmail.com>
2025-06-01 13:45:22 +0700

    Update roles

--- app/Http/Controllers/UKMController.php
+++ app/Http/Controllers/UKMController.php
@@ -8,24 +8,4 @@ use App\Models\User;
 class UKMController extends Controller
 {
     //
-
-    // verifikasi ukm
-    public function verify(Request $request)
-    {
-        $request->validate([
-            'ukm_id' => 'required|exists:users,id'
-        ]);
-
-        $user = User::find($request->ukm_id);
-        
-        if ($user->isUkm()) {
-            $user->update(['is_verified' => true]);
-            
-            // Kirim notifikasi ke UKM bahwa akun mereka terverifikasi
-            // ...
-            return back()->with('success', 'Akun UKM berhasil diverifikasi');
-        }
-
-        return back()->with('error', 'Hanya akun penyelenggara yang dapat diverifikasi');
-    }
 }
