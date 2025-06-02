d3a9ff22c269d5b52c01159502e90f3a2ec1046a
Lola Anggita <suchcemeann@gmail.com>
2025-06-02 15:05:37 +0700

    renew penyesuaian migration agar bisa migrate:fresh

--- database/migrations/2025_05_25_205948_create_u_k_m_s_table.php
+++ database/migrations/2025_05_25_205948_create_u_k_m_s_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
              $table->string('name');
             $table->text('description');
-            $table->string('logo')->nullable();
+           // $table->string('logo')->nullable();
             $table->string('contact')->nullable();
             $table->timestamps();
         });
